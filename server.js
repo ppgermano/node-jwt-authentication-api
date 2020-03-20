28e175e8c9f5509312a81127770aa81a5df3053d
Pedro Pegoraro Germano <pedrogermano@indg.com.br>
2020-03-20 09:52:16 -0300

    endpoint /no-auth criado

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// No security
+app.get('/no-auth', (req, res) => {
+    res.status(200).send({ status: 'Success', data: 'xyz' });
+});
+
 // use JWT auth to secure the api
 app.use(jwt());
 
@@ -21,6 +26,6 @@ app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
+const server = app.listen(port, function() {
     console.log('Server listening on port ' + port);
 });
--- users/user.service.js
+++ users/user.service.js
@@ -2,7 +2,15 @@
 const jwt = require('jsonwebtoken');
 
 // users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const users = [
+    {
+        id: 1,
+        username: 'user',
+        password: 'passwd',
+        firstName: 'Test',
+        lastName: 'User'
+    }
+];
 
 module.exports = {
     authenticate,
@@ -10,7 +18,9 @@ module.exports = {
 };
 
 async function authenticate({ username, password }) {
-    const user = users.find(u => u.username === username && u.password === password);
+    const user = users.find(
+        u => u.username === username && u.password === password
+    );
     if (user) {
         const token = jwt.sign({ sub: user.id }, config.secret);
         const { password, ...userWithoutPassword } = user;
